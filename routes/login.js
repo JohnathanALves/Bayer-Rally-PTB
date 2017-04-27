5170e31a62154dc309dab7156968cea5a146f733
Johnathan Alves <jonata94@gmail.com>
2017-04-27 04:27:59 -0300

    logout route

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,42 @@
 var express = require('express');
 var router  = express.Router();
 
+var isAuthenticated = function (req, res, next) {
+		if (req.isAuthenticated())
+		return res.json({status: 'true'});
+	// if the user is not authenticated then redirect him to the login page
+	return res.json({status: 'false'});
+}
 
 module.exports = function(passport){
 
+  router.get('/islog', function(req, res, next){
+    if(req.isAuthenticated()){
+      return res.json({status:'true'});
+    } else {
+      return res.json({status:'false'});
+    }
+  });
 
+  router.get('/isvendedor', function(req, res, next){
+    if(req.isAuthenticated()){
+      if(req.user.isVendedor){
+        return res.json({status:'true'});
+      } else {
+        return res.json({status:'false'});
+      }
+    }
+  });
 
   router.post('/',  passport.authenticate('login', {
     successRedirect : 'login/successJson',
     failureRedirect : 'login/failuresJson',
   }));
+
+  router.get('/logout', function(req, res, next){
+    req.logout();
+    res.redirect('/');
+  });
 
   router.get('/successJson', function(req, res, next){
     return res.json({status: 'true'});
@@ -19,9 +46,12 @@ module.exports = function(passport){
     return res.json({status: 'false'});
   });
 
+  router.get('/successJson', function(req, res, next){
+    return res.json({status: 'true'});
+  });
+
   router.get('/teste', function(req, res, next){
-    var testeController = require('../controllers/teste');
-    testeController(req, res, next);
+    var testeController = require('../controllers/teste')(req, res, next);
   });
   return router;
 }
